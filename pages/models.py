429ecb42b9025de05c08ddeec0691573341b86e5
Lyman <lyman.lu@outlook.co.nz>
2026-04-09 14:01:34 +1200

    gitignore changes

--- pages/models.py
+++ pages/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Member():
+    first_name = models.CharField(max_length=100)
+    surname = models.CharField(max_length=100)
+    school = models.CharField(max_length=100)
+    year_level = models.IntegerField()
+    email = models.EmailField(unique=True)
+
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
 def IndexView(request):
-    return render(request, 'index.html')
+    return render(request, 'pages/index.html')
 
