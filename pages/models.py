b63fb0eb6f8fba2933ece9e5bfd9ce67c54af7e3
Lyman <lyman.lu@outlook.co.nz>
2026-04-09 14:26:19 +1200

    i don't even know what I changed

--- pages/models.py
+++ pages/models.py
@@ -1,9 +1,26 @@
 from django.db import models
 
-class Member():
+class Member(models.Model):
+    SCHOOLS = [
+        ('AGS', 'Auckland Grammar School'),
+        ('STC', 'St Cuthbert\'s College'),
+        ('STK', 'St Kent\'s College'),
+        ('BAR', 'Baradene College'),
+        ('EGG', 'Epsom Girl\'s College'),
+        ('KC', 'King\'s College'),
+        ('GDC', 'Glendowie College'),
+        ('Selwyn', 'Selwyn College'),
+    ]
+
     first_name = models.CharField(max_length=100)
-    surname = models.CharField(max_length=100)
-    school = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    school = models.CharField(
+        max_length=100,
+        choices=SCHOOLS
+    )
     year_level = models.IntegerField()
     email = models.EmailField(unique=True)
+
+    def __str__(self):
+        return self.first_name + " " + self.last_name
 
