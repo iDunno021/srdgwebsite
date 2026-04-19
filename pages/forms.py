136a965807cab67f232b3ee06cf28c688ddb4d3b
iDunno021 <lyman.lu@outlook.co.nz>
2026-04-20 09:44:18 +1200

    Remove year_level widget customization

--- pages/forms.py
+++ pages/forms.py
@@ -5,6 +5,3 @@ class MemberForm(forms.ModelForm):
     class Meta:
         model = Member
         fields = ['first_name', 'last_name', 'school', 'year_level', 'email']
-        widgets = {
-            'year_level': forms.NumberInput(attrs={'min': 9, 'max': 13})
-        }
