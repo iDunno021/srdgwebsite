193da77ef94dd2c1a521d847883367f9e49f6f2e
Shreyash <273081884+shreyash0823-wq@users.noreply.github.com>
2026-05-05 19:47:48 +1200

    add optional discord_username field to Member model

--- pages/models.py
+++ pages/models.py
@@ -32,6 +32,7 @@ class Member(models.Model):
     school = models.CharField(max_length=100, choices=SCHOOLS)
     year_level = models.IntegerField(choices=YEAR_CHOICES)
     email = models.EmailField(unique=True)
+    discord_username = models.CharField(max_length=100, blank=True, null=True)
 
     def __str__(self):
         return self.first_name + " " + self.last_name
