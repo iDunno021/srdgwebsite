4186e45a90d40164bc62ea25f849565bfb229a96
Lyman <lyman.lu@outlook.co.nz>
2026-07-12 11:26:08 +1200

    Add network263 page with corresponding template and styles

--- pages/views.py
+++ pages/views.py
@@ -230,3 +230,6 @@ def partners(request):
         'total_members': total_members,
         'schools_count': schools_count,
     })
+
+def network263(request):
+    return render(request, 'pages/network263.html')
