86ecffb4d809106c8abdcb42fc30b51580456df3
Alex Shan <Bing@Alex-743.local>
2026-07-13 22:34:41 +1200

    fix allowed hosts for local dev

--- website/settings.py
+++ website/settings.py
@@ -29,8 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'srdg.co.nz,www.srdg.co.nz').split(',')
-
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'srdg.co.nz,www.srdg.co.nz').split(',') + ['localhost', '127.0.0.1']
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
